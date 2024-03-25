eb397b03300f7bbcd376dc14edfdc80400cce395
Guilherme Alves <iamalves.jpg@gmail.com>
2024-03-25 17:08:16 -0300

    register(feat):add register function

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -2,14 +2,44 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Eye, EyeOff } from 'lucide-react'
 import { useState } from "react";
+import { z } from 'zod'
+import {useForm} from 'react-hook-form'
+import {zodResolver} from '@hookform/resolvers/zod'
+import { useFirebase } from "@/services/firebase";
+import { createUserWithEmailAndPassword, sendEmailVerification, updateProfile } from 'firebase/auth'
+
+const userSchema = z.object({ 
+    username: z.string(),
+    password: z.string(),
+    confirm_password: z.string(),
+    email: z.string().email("Digite um email valido!")
+ }).refine(({ password, confirm_password}) => password === confirm_password, {
+    message: "Password doesn't match",
+    path: ["confirm_password"]
+})
+
+type UserData = z.infer<typeof userSchema>
 
 export default function Register() {
+    const { auth } = useFirebase()
+
+    const {register, handleSubmit} = useForm<UserData>({
+        resolver: zodResolver(userSchema)
+    })
 
     const [show, setShow] = useState(false);
 
     function handleShow(){
         setShow(!show)
     }
+
+    async function handleRegister(data: UserData){
+        const response = await createUserWithEmailAndPassword(auth, data.email, data.password)    
+        sendEmailVerification(response.user)
+        updateProfile(response.user, {displayName: data.username})
+       
+    }
+    
 
   return (
     <div className="h-screen max-h-screen flex flex-col justify-center items-center">
@@ -18,22 +48,24 @@ export default function Register() {
             <span className="text-3xl font-bold text-comp">fy</span>
         </div>
         
-        <div className="border p-4 shadow-md rounded md:w-1/5 flex flex-col justify-center mt-4">
+        <form onSubmit={handleSubmit(handleRegister)} className="border p-4 shadow-md rounded sm:w-1/5 flex flex-col justify-center mt-4">
             <span className="mb-4 font-bold">Nome de úsuario: </span>
             <Input
                 placeholder="Digite seu nome de úsuario"
+                {...register("username")}
             />
             <span className="my-4 font-bold">Email: </span>
             <Input
                 placeholder="Digite seu email"
                 type="email"
+                {...register("email")}
             />
             <span className="my-4 font-bold">Senha: </span>
             <div className="flex items-center gap-2">
                 <Input
                     placeholder="Digite sua senha"
                     type={show ? "text" : "password"}
-                    
+                    {...register("password")}
                 />
                 {
                     show ? (
@@ -47,11 +79,12 @@ export default function Register() {
             <Input
                 placeholder="Digite sua senha"
                 type={show ? "text" : "password"}
+                {...register("confirm_password")}
             />
             
-            <Button className="mt-4 font-bold">Cadastrar</Button>
+            <Button className="mt-4 font-bold" type="submit">Cadastrar</Button>
             <a href="/login" className="text-sm mt-2 text-black/50 text-center hover:underline">Já possui uma conta? Entre agora mesmo!</a>
-        </div>
+        </form>
     </div>
   )
 }
